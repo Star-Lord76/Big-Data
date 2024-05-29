0a2ddeb6ed32419c5a5a1fb25813ca5a1328bbed
Domenico Chianese <nico.chianese00@gmail.com>
2024-05-29 20:04:55 +0200

    Fine Dica33Scraping

--- dire33Utils.py
+++ dire33Utils.py
@@ -31,41 +31,41 @@ def getQandA(multiLineString: str) -> list[str]:
     return (splittedString[5], splittedString[12])
 
 
-def createSetPickleSnapshot(setToSnapshot: set, path: str) -> bool:
+def createObjectPickleSnapshot(objectToSnapshot: any, path: str) -> bool:
     """
-    The function `createSetPickleSnapshot` takes a set and a file path as input, then saves a snapshot
+    The function `createObjectPickleSnapshot` takes an object and a file path as input, then saves a snapshot
     of the set to a pickle file at the specified path.
 
-    :param setToSnapshot: The `setToSnapshot` parameter is a set that you want to create a snapshot of.
+    :param setToSnapshot: The `objectToSnapshot` parameter is a set that you want to create a snapshot of.
     This function takes this set and saves it to a file using the Python `pickle` module. The `path`
     parameter specifies the file path where the snapshot will be saved
     :type setToSnapshot: set
-    :param path: The `path` parameter in the `createSetPickleSnapshot` function is a string that
+    :param path: The `path` parameter in the `createObjectPickleSnapshot` function is a string that
     represents the file path where the snapshot of the set will be saved as a pickle file
     :type path: str
-    :return: The function `createSetPickleSnapshot` returns a boolean value. It returns `True` if the
+    :return: The function `createObjectPickleSnapshot` returns a boolean value. It returns `True` if the
     set `setToSnapshot` is successfully pickled and saved to the specified path, and it returns `False`
     if there is an exception raised during the process.
     """
     try:
         with open(path, 'wb') as f:
-            pickle.dump(setToSnapshot, f)
+            pickle.dump(objectToSnapshot, f)
     except:
         return False
     return True
 
 
-def loadSetPickleSnapshot(path: str) -> set:
+def loadObjectPickleSnapshot(path: str) -> any:
     """
-    The function `loadSetPickleSnapshot` loads a set object from a pickle file at the specified path.
+    The function `loadObjectPickleSnapshot` loads a set object from a pickle file at the specified path.
 
-    :param path: The `path` parameter in the `loadSetPickleSnapshot` function is a string that
+    :param path: The `path` parameter in the `loadObjectPickleSnapshot` function is a string that
     represents the file path to the pickle file that contains the snapshot of a set object. This
     function reads the pickle file and returns the set object stored in it
     :type path: str
-    :return: The function `loadSetPickleSnapshot` is returning a set object that is loaded from a pickle
+    :return: The function `loadSetPickleSnapshot` is returning an object that is loaded from a pickle
     file located at the specified `path`.
     """
     with open(path, 'rb') as f:
-        pickleLinkSet = pickle.load(f)
-    return pickleLinkSet
+        pickleLinkObject = pickle.load(f)
+    return pickleLinkObject
